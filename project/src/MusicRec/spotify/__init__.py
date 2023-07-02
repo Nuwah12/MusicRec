f9e899e25fafecc4c699320a364cffcf53a14f76
Nuwah12 <nburget1@hotmail.com>
2023-07-01 23:45:14 -0400

    init

--- project/src/MusicRec/spotify/__init__.py
+++ project/src/MusicRec/spotify/__init__.py
@@ -2,3 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy.util as util
+
+# This might be bad practice? 
+# Might want to reference functions directly instead of *
+from .user_auth import UserAuth 
+from .fetch_playlists_songs import Fetcher 
