73058ef8308ca6ade5ffb47f765db3954193c969
Nuwah12 <nburget1@hotmail.com>
2023-07-03 22:20:05 -0400

    Spotify api JSON processing

--- project/src/MusicRec/spotify/fetch_playlists_songs.py
+++ project/src/MusicRec/spotify/fetch_playlists_songs.py
@@ -1,19 +1,32 @@
+import pandas as pd
 class SpotifySearch():
-    def __init__(self):
-        pass
+    def __init__(self, user, sp):
+        self.user = user
+        self.sp = sp
     # Get songs from playlist URI and user name
-    def get_songs_from_playlistURL(plURL, user, sp):
+    def _process_api_call(self, pl_data, instance='playlist'):
         track_ids = []
         track_names = []
-        tracks = sp.user_playlist(user, plURL)['tracks']['items']
-
-        for i in range(0, len(tracks)):
-            track_ids.append(tracks[i]['track']['id'])
-            track_names.append(tracks[i]['track']['name'])
-
+        tracks = pl_data['tracks']['items']
+        for i in range(0,len(tracks)):
+            if instance=='tracks':
+                track_ids.append(tracks[i]['id'])
+                track_names.append(tracks[i]['name'])
+            else:
+                track_ids.append(tracks[i]['track']['id'])
+                track_names.append(tracks[i]['track']['name'])
         all_songs = pd.DataFrame({'id': track_ids, 'names': track_names})
-        song_names = all_songs['names']
         return all_songs
+    def get_songs_from_playlistURL(self, plURL):
+        pl = self.sp.user_playlist(self.user, plURL)
+        pl=self._process_api_call(pl)
+        return pl
+    def get_playlist_by_genre(self, genre):
+        songs = self.sp.search('genre:{}'.format(genre), limit=50, type='track')
+        if len(songs) == 0:
+            raise ValueError('Genre did not return any results ... try different genre')
+        songs=self._process_api_call(songs,instance='tracks')
+        return songs
     # Get song feautures
     def get_song_features(all_songs, sp):
         features_df = pd.DataFrame()
